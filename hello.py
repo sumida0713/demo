8b55762b2b03628903c3b6f0cbe6d082d899d0e8
sumida0713 <k.sumida@vamps-net.com>
2021-09-05 16:58:31 +0900

    test2 hogehoge

--- hello.py
+++ hello.py
@@ -1,14 +1,5 @@
-import sys
 
-print(sys.version)
+def get_hoge(**args):
+    print(args)
 
-
-x = 2
-y = 2
-
-if x > y:
-    print('Xがおおきい')
-elif y > x:
-    print('Yがおおきい')
-else:
-    print('同じ')
+get_hoge(name='sumida', age=39)
